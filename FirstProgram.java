30a7c504e3b31c667517912f76970b1dd5ab8804
ashok <vardinenia@gmail.com>
2024-01-24 19:34:48 +0530

    first java program

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+class FirstProgram
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		String s="java is good programmming language";
+		String[] s1=s.split(" ");
+		for(String s2: s1){
+			System.out.println(s2);
+		}
+			}
+}
